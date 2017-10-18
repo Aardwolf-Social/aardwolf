060285cf67b504b6d2a16cd9f117ce51e449b047
Elijah Mark Anderson <kd0bpv@gmail.com>
2017-10-18 11:07:38 -0500

    Start filling out skeleton of main module

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // TODO: Parse command-line arguments
+    // Set defaults for command line arguments
+    
+    // Process and shadow defaults.
+    
+    // Todo: Parse Config
+    
+    // Initialize/Fork
+    
+    // Main loop
+    let run = true;
+    while run {
+        
+    }
+}
