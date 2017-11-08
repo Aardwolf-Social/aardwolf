8f27b1491cb1e34da4ef534cb6a7d04f24b61dc3
Banjo Fox <drbanjofox@protonmail.com>
2017-11-08 10:29:13 -0500

    Adding some new directories to web_ui, as well as Rocket's "hello world" cargo

---
+++ web_ui/rocket/src/main.rs
@@ -0,0 +1,15 @@
+# Boilerplate Rocket Hello World ;D
+
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
