bd9c773ec91b37c2a15d6337afd9a12abc3691f2
Elijah Mark Anderson <mark@kd0bpv.name>
2017-12-17 22:44:12 -0600

    Fix panic at DATABASE_URL not being set

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,16 +25,35 @@ use std::path::PathBuf;
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 fn db_pool(rocket: &Rocket) -> Pool {
-    // TODO: Convert this to build the database_url using values from the app config.
-    let database_url = rocket.config().get_str("database_url").expect("Must set DATABASE_URL");
+    let database_url = rocket.config().get_str("database_url").expect("Must set Database.url in config");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder().build(manager).expect("Could not get DB connection pool")
 }
 
 fn app(config: config::Config) -> Rocket {
+    let mut db_url = String::new();
+    match config.get_str("Database.type").unwrap().to_lowercase().as_str() {
+        "postgresql" => db_url.push_str("postgres://"),
+
+        // If we reach this case, it's an error.
+        // TODO: Handle it.
+        _ => {}
+    }
+
+    db_url.push_str(config.get_str("Database.username").unwrap().as_str());
+    db_url.push(':');
+    db_url.push_str(config.get_str("Database.password").unwrap().as_str());
+    db_url.push('@');
+    db_url.push_str(config.get_str("Database.host").unwrap().as_str());
+    db_url.push(':');
+    db_url.push_str(config.get_str("Database.port").unwrap().as_str());
+    db_url.push('/');
+    db_url.push_str(config.get_str("Database.database").unwrap().as_str());
+
     let c = rocket::Config::build(rocket::config::Environment::Development)
         .address(config.get_str("Listen.address").unwrap())
         .port(config.get::<u16>("Listen.port").unwrap())
+        .extra("database_url", db_url.as_str())
         .unwrap();
 
     let r = rocket::custom(c, true)
@@ -64,9 +83,9 @@ fn app(config: config::Config) -> Rocket {
 fn main() {
     // Set defaults
     let mut config = Config::default();
-    config.set_default::<&str>("cfg_file", "/etc/aardwolf/config.toml").unwrap();
+    config.set_default::<&str>("cfg_file", "aardwolf.toml").unwrap();
     config.set_default::<&str>("Listen.address", "127.0.0.1").unwrap();
-    config.set_default("Listen.port", 7878);
+    config.set_default("Listen.port", 7878).unwrap();
 
     // Merge environment variables
     config.merge(Environment::with_prefix("aardwolf")).unwrap();
