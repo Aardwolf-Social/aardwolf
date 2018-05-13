8a2f6acc88ad38ef1d8298157a50dcb931b47d39
Elijah Mark Anderson <mark@kd0bpv.name>
2018-05-13 12:14:44 -0500

    Add basic build script
    Define log-syslog and use-systemd features in Cargo.toml

---
+++ build.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+ read_config();
+ let config_changed = configure();
+ if config_changed {write_config();}
+ validate_features();
+}
+
+/* Looks for a .config file. If found, reads it's values into the 
+ * environment for re-use. If a value is already set, it will skip setting 
+ * it and defer to the new value.
+ */
+fn read_config() {
+
+}
+
+/* Goes through the build configuration values, tries to auto-detect
+ * missing values, and checks that re-used values are still valid.
+ * Returns: true if any values have changed from .config, false otherwise.
+ */
+fn configure() -> bool {
+    let config_changed = false;
+    
+    config_changed
+}
+
+/* Writess out the configuration to .config for re-use next build.
+ */
+fn write_config() {
+
+}
+
+/* Checks that incompatible features haven't been selected.
+ * Aborts build if so.
+ */
+fn validate_features() {
+
+}
