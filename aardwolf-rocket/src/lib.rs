7f58584e0e20bcc796f5c107447ba399373f3956
asonix <asonix@asonix.dog>
2018-11-08 17:09:55 -0600

    Start manually fixing things

--- aardwolf-rocket/src/lib.rs
+++ aardwolf-rocket/src/lib.rs
@@ -18,6 +18,7 @@ extern crate r2d2;
 extern crate r2d2_diesel;
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate rocket_i18n;
 extern crate serde;
 
 use diesel::pg::PgConnection;
@@ -111,8 +112,14 @@ fn app(config: config::Config, db_url: String) -> Result<Rocket, Box<dyn Error>>
             routes![self::routes::applications::register_application],
         )
         .mount("/", routes)
-        .attach(Template::fairing());
-    // .manage(SystemRandom::new());
+        // .manage(SystemRandom::new());
+        // Just for giggles, what happens if I put the rocket_i18n fairing here....
+        // Register the fairing. The parameter is the domain you want to use (the name of your app most of the time)
+        .attach(rocket_i18n::I18n::new("aardwolf"))
+        // Eventually register the Tera filters (only works with the master branch of Rocket)
+        .attach(rocket_contrib::Template::custom(|engines| {
+            rocket_i18n::tera(&mut engines.tera);
+        }));
 
     // we need an instance of the app to access the config values in Rocket.toml,
     // so we pass it to the db_pool function, get the pool, and _then_ return the instance
