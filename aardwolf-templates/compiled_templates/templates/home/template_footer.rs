2911249600743def38b1a35d1e9c73ef922c2034
Banjo Fox <drbanjofox@protonmail.com>
2018-11-30 11:10:30 -0500

    Standardizing icons with <span>

--- aardwolf-templates/compiled_templates/templates/home/template_footer.rs
+++ aardwolf-templates/compiled_templates/templates/home/template_footer.rs
@@ -14,8 +14,8 @@ write!(out, "</a>\n\t\t<span class=\"vertical_line\"/>\n\t\t<span class=\"footer
 i18n!(catalog, "Copyright 2018").to_html(out)?;
 write!(out, "</span>\n\t\t<span class=\"vertical_line\" />\n\t\t<a href=\"https://github.com/BanjoFox/aardwolf\" class=\"footer_box\"> ")?;
 i18n!(catalog, "Check us out on GitHub").to_html(out)?;
-write!(out, "<i class=\"fab fa-github\"></i>!</a>\n\t\t<span class=\"vertical_line\" />\n\t\t<a href=\"https://www.patreon.com/banjofox\" class=\"footer_box\">")?;
+write!(out, "<span class=\"fa fa-github\"></span>!</a>\n\t\t<span class=\"vertical_line\" />\n\t\t<a href=\"https://www.patreon.com/banjofox\" class=\"footer_box\">")?;
 i18n!(catalog, "Buy the team a coffee").to_html(out)?;
-write!(out, "<span class=\"fas fa-beer\"aria-hidden=\"true\"></span></a>\t\t\n\t</div>\n  </div>\n</footer>\n")?;
+write!(out, "<span class=\"fa fa-beer\"aria-hidden=\"true\"></span></a>\t\t\n\t</div>\n  </div>\n</footer>\n")?;
 Ok(())
 }
