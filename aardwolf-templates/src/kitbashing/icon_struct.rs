c305498adfd8f3e2f558ef09bf82355d59b2f277
Banjo Fox <drbanjofox@protonmail.com>
2018-12-01 20:12:44 -0500

    adding the icon_struct.rs

---
+++ aardwolf-templates/src/kitbashing/icon_struct.rs
@@ -0,0 +1,125 @@
+impl Icon {    
+			pub fn angle-down() -> Self {
+				IconName::AngleDown,            			
+				size: IconSize::Normal,        
+			}
+			pub fn beer() -> Self {
+				IconName::Beer,            				
+				size: IconSize::Normal,
+			}
+			pub fn bell() -> Self {			            				
+				IconName::Bell, 
+				size: IconSize::Normal,
+			}
+			pub fn bookmark() -> Self {
+				IconName::Bookmark,            				
+				size: IconSize::Normal,
+			}
+			pub fn calendar() -> Self {
+				IconName::Calendar,            				
+				size: IconSize::Normal,
+			}
+			pub fn calendar-plus-o() -> Self {
+				IconName::CalendarPlusOutline,            				
+				size: IconSize::Normal,
+			}
+			pub fn chevron-left() -> Self {
+				IconName::ChevronLeft,            				
+				size: IconSize::Normal,
+			}
+			pub fn cloud() -> Self {
+				IconName::Cloud,            				
+				size: IconSize::Normal,
+			}
+			pub fn cloud-download() -> Self {
+				IconName::CloudDownload,            				
+				size: IconSize::Normal,
+			}
+			pub fn cloud-upload() -> Self {
+				IconName::CloudUpload,            				
+				size: IconSize::Normal,
+			}
+			pub fn ellipsis-h() -> Self {
+				IconName::EllipsisH,            				
+				size: IconSize::Normal,
+			}
+			pub fn envelope() -> Self {
+				IconName::Envelope,            				
+				size: IconSize::Normal,
+			}
+			pub fn gears() -> Self {
+				IconName::Gears,            				
+				size: IconSize::Normal,
+			}
+			pub fn github() -> Self {
+				IconName::Github,            				
+				size: IconSize::Normal,
+			}
+			pub fn lightbulb-o() -> Self {
+				IconName::LightbulbOutline,            				
+				size: IconSize::Normal,
+			}
+			pub fn list() -> Self {
+				IconName::List,            				
+				size: IconSize::Normal,
+			}
+			pub fn logout() -> Self {
+				IconName::Logout,            				
+				size: IconSize::Normal,
+			}
+			pub fn lok() -> Self {          				
+				IconName::Lok,
+				size: IconSize::Normal,
+			}
+			pub fn newspaper() -> Self {
+				IconName::Newspaper,            				
+				size: IconSize::Normal,
+			}
+			pub fn newspaper-o() -> Self {
+				IconName::NewspaperOutline,            				
+				size: IconSize::Normal,
+			}
+			pub fn picture-o() -> Self {
+				IconName::PictureOutline,            				
+				size: IconSize::Normal,
+			}
+			pub fn sliders() -> Self {
+				IconName::Sliders,            				
+				size: IconSize::Normal,
+			}
+			pub fn star() -> Self {
+				IconName::Star,            				
+				size: IconSize::Normal,
+			}
+			pub fn upload() -> Self {
+				IconName::Upload,            				
+				size: IconSize::Normal,
+			}
+			pub fn user() -> Self {
+				IconName::User,            				
+				size: IconSize::Normal,
+			}
+			pub fn user-circle() -> Self {
+				IconName::UserCircle,            				
+				size: IconSize::Normal,
+			}
+			pub fn user-plus() -> Self {
+				IconName::UserPlus,            				
+				size: IconSize::Normal,
+			}
+			pub fn users() -> Self {
+				IconName::Users,            				
+				size: IconSize::Normal,
+			}
+			pub fn volume-off() -> Self {
+				IconName::VolumeOff,            				
+				size: IconSize::Normal,
+			}
+			
+    	pub fn large(mut self) -> Self {
+        self.size = IconSize::Large
+        self
+    }
+
+    ... //  What is supposed to go here? 
+}			
--- aardwolf-templates/src/lib.rs
+++ aardwolf-templates/src/lib.rs
@@ -4,10 +4,10 @@ extern crate rocket_i18n;
 include!("../compiled_templates/templates.rs");
 
 mod home;
+mod kitbashing;
 mod shortcuts;
 mod sign_in;
 mod sign_up;
-mod ui;
 
 pub use self::{home::Home, sign_in::SignIn, sign_up::SignUp};
 
