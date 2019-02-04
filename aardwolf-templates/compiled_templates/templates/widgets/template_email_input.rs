4171ff0286ed26c81a011663cd6f23b09128cf9f
Banjo Fox <4459812+BanjoFox@users.noreply.github.com>
2019-02-04 08:41:53 -0500

    Switch ui:: to widgets::

--- aardwolf-templates/compiled_templates/templates/widgets/template_email_input.rs
+++ aardwolf-templates/compiled_templates/templates/widgets/template_email_input.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 #[cfg_attr(feature="cargo-clippy", allow(useless_attribute))]
 #[allow(unused)]
 use super::{Html,ToHtml};
-use crate::{EmailInput, templates::ui::input};
+use crate::{EmailInput, templates::widgets::input};
 
 pub fn email_input(out: &mut Write, email_input: &EmailInput) -> io::Result<()> {
 input(out, &email_input.into())?;
