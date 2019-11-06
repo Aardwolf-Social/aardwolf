8d30880c30693ce32e631ba96527949fce5b59b0
Peter Alexander <peter.alexander@goevo.com>
2019-11-06 11:34:55 -0500

    Start updating templates from aardwolf-interface repo

---
+++ web/javascript/components/delete_notifcation.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
