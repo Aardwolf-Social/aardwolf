1285f5662402203fd22f4b2e7d9210832955ebda
Banjo Fox <drbanjofox@protonmail.com>
2023-06-27 16:43:05 -0400

    Swap yaml_rust for toml crate

---
+++ src/bin/aardwolf-server-toml.rs
@@ -0,0 +1,51 @@
+use std::error::Error;
+
+use aardwolf::{begin_log, configure};
+use clap::{load_yaml, App};
+use config::Config;
+use toml::from_str;
+
+fn cli<'a, 'b>(toml: &'a str) -> App<'a, 'b> {
+    let table: toml::value::Table = toml.parse().unwrap();
+    App::from(table)
+        .name(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+}
+
+pub trait AardwolfServer {
+    fn run(&self, config: &Config, db_url: &str) -> Result<(), Box<dyn Error>>;
+}
+
+#[cfg(feature = "actix")]
+mod actix {
+    use config::Config;
+    use std::error::Error;
+
+    use super::AardwolfServer;
+
+    pub struct Server;
+
+    impl AardwolfServer for Server {
+        fn run(&self, config: &Config, db_url: &str) -> Result<(), Box<dyn Error>> {
+            aardwolf_actix::run(config, db_url)
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let toml = include_str!("cli.toml");
+    let cli = cli(&toml);
+    let config = configure(cli)?;
+    let db_url = aardwolf::db_conn_string(&config)?;
+
+    begin_log(&config);
+
+    #[cfg(feature = "actix")]
+    let server = actix::Server;
+
+    server.run(&config, &db_url)?;
+
+    Ok(())
+}
