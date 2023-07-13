9a495f4e7542029b868cd6a05fc3d67246e228ac
Banjo Fox <banjofox@c02yk2a9jgh5.mynetworksettings.com>
2023-07-12 22:04:43 -0400

    Switching to  CDN copies of Bulma + ForkAwesome, also fixing stupid syntax problems in CSS.

--- aardwolf-yew/src/templates/layout/footer.rs
+++ aardwolf-yew/src/templates/layout/footer.rs
@@ -8,13 +8,13 @@ pub fn footer() -> Html {
             <footer class="footer">
                 <div class="container">
                 <div class="content has-text-centered">
-                    <a href="termsofservice.html" class="footer_box">{" Terms of Service "}</a>
-                    <span class="vertical_line" />
-                    <span class="footer_box">{" Copyright 2018 "}</span>
-                    <span class="vertical_line" />
-                    <a href="https://github.com/BanjoFox/aardwolf" class="footer_box"> {" Check us out on GitHub "}<i class="fa fa-github"></i>{"!"}</a>
-                    <span class="vertical_line" />
-                    <a href="https://www.patreon.com/banjofox" class="footer_box">{" Buy the team a coffee "}<i class="fa fa-beer" aria-hidden="true"></i></a>		
+                    <a href="termsofservice.html" class="footer-box">{" Terms of Service "}</a>
+                    <span class="vertical-line" />
+                    <span class="footer-box">{" Copyright 2018 "}</span>
+                    <span class="vertical-line" />
+                    <a href="https://github.com/BanjoFox/aardwolf" class="footer-box"> {" Check us out on GitHub "}<i class="fa fa-github"></i>{"!"}</a>
+                    <span class="vertical-line" />
+                    <a href="https://www.patreon.com/banjofox" class="footer-box">{" Buy the team a coffee "}<i class="fa fa-beer" aria-hidden="true"></i></a>		
                 </div>
                 </div>
             </footer>
