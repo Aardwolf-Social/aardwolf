1d6d3348c7f4060aee4ad9dfbf16186373790b67
Banjo Fox <drbanjofox@protonmail.com>
2023-08-02 12:17:29 -0400

    Removing dead frontend code, and cleaning up /src/lib.rs a bit

--- web/javascript/components/delete_notifcation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    $notification = $delete.parentNode;
-    $delete.addEventListener('click', () => {
-      $notification.parentNode.removeChild($notification);
-    });
-  });
-});
