235d6dd9ccd87478c4c465236508b47503e0e4f2
Banjo Fox <drbanjofox@protonmail.com>
2023-08-08 18:08:36 -0400

    Do some more testing, send help.

--- aardwolf-yew-frontend/src/templates/shared/about_us.rs
+++ aardwolf-yew-frontend/src/templates/shared/about_us.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use rust_i18n::t;
 
+
 #[function_component(AboutUs)]
 pub fn about_us() -> Html {
 
@@ -10,10 +11,10 @@ pub fn about_us() -> Html {
         <>
             <div class="column is-mobile">
                 <h1 class="title">
-                    {t!("About Aardwolf")}
+                    {title}
                 </h1>
                 <p class="subtitle">
-                    {t!("This is who we are!")}
+                    {t!("about_aardwolf_subtitle")}
                 </p>
                 <p>{t!("Aardwolf is a platform for creating new social networks, connected across the web. While existing social media sites work to funnel the world into a single shared experience (and advertising marketplace), we recognize that we are all individuals with different identities and interests.")}</p><br />
                 <p>{t!("Aardwolf is a platform for creating new social networks, connected across the web. While existing social media sites work to funnel the world into a single shared experience (and advertising marketplace), we recognize that we are all individuals with different identities and interests.")}</p><br />
@@ -21,7 +22,7 @@ pub fn about_us() -> Html {
                 <p>{t!("Each server hosts it's own community of users who are posting, sharing pictures, links, etc. They are replying and liking each other's posts, and re-sharing the ones they like best.")}</p><br />
                 <p>{t!("Each server hosts it's own community of users who are posting, sharing pictures, links, etc. They are replying and liking each other's posts, and re-sharing the ones they like best.")}</p><br />
 
-                <p>{("Users are not limited to only interacting with other users on their service: they can follow users on other sites that are powered by Aardwolf just as if they were on their own site (the official term is Federation). They can even connect with users on other platforms, if -- like the microblogging service")} <a href="http://joinmastodon.org">{" Mastodon "}</a> {t!("-- they implement the same open protocols Aardwolf is built on.")}</p><br />
+                <p>{t!("Users are not limited to only interacting with other users on their service: they can follow users on other sites that are powered by Aardwolf just as if they were on their own site (the official term is Federation). They can even connect with users on other platforms, if they implement the same open protocols Aardwolf is built on.")}</p><br />
 
                 <p><a href="https://github.com/banjofox/aardwolf">{"Aardwolf "}</a> {t!("is open source, so developers who want to contribute or understand how it works can dig in and do so.")}</p>
                 <p><a href="https://github.com/banjofox/aardwolf">{"Aardwolf "}</a> {t!("is open source, so developers who want to contribute or understand how it works can dig in and do so.")}</p>
