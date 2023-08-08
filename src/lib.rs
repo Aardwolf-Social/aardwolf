a7d0a19cabfd5e1ce132be0c1bd1f1409a720aca
maddiebaka <madeline@cray.lgbt>
2023-08-07 22:50:26 -0400

    Fix compiler warnings in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::env;
 use anyhow::{Context, Result};
 use clap::Parser;
 use clap_verbosity_flag::Verbosity;
-use config::{Config, ConfigError, Environment};
+use config::{Config, Environment, File, FileFormat};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,56 +19,69 @@ pub struct Args {
 
 pub fn configure(parsed_args: Args) -> Result<Config> {
     // Set defaults
-    let mut config = Config::default();
-    config
-        .set_default::<&str>("cfg_file", concat!(env!("CARGO_PKG_NAME"), ".toml"))
+    let mut builder = Config::builder();
+    builder = builder
+        .set_default("cfg_file", concat!(env!("CARGO_PKG_NAME"), ".toml"))
         .context(ErrorKind::ConfigImmutable)?;
-    config
-        .set_default::<&str>("Log.file", "_CONSOLE_")
+    builder = builder
+        .set_default("Log.file", "_CONSOLE_")
         .context(ErrorKind::ConfigImmutable)?;
-    config
-        .set_default::<&str>("Web.address", "127.0.0.1")
+    builder = builder
+        .set_default("Web.address", "127.0.0.1")
         .context(ErrorKind::ConfigImmutable)?;
-    config
+    builder = builder
         .set_default("Web.port", 7878)
         .context(ErrorKind::ConfigImmutable)?;
 
     // Determine config file
-    if let Ok(config_file) = env::var("AARDWOLF_CONFIG") {
-        config
-            .set("cfg_file", config_file)
+    let config_file = env::var("AARDWOLF_CONFIG").unwrap();
+    builder = builder
+        .set_override("cfg_file", config_file.clone())
+        .context(ErrorKind::ConfigImmutable)?;
+
+    builder = builder.add_source(File::new(&config_file, FileFormat::Json)); // Or whatever format
+
+    let config_path = parsed_args.config.unwrap();
+        builder = builder
+            .set_override("cfg_file", config_path.to_str())
             .context(ErrorKind::ConfigImmutable)?;
-    }
+    //}
 
-    if let Some(config_path) = parsed_args.config {
-        config
-            .set("cfg_file", config_path.to_str())
-            .context(ErrorKind::ConfigImmutable)?;
-    }
-
-    // Merge config file and apply overrides
-    let config_file_string = config
-        .get_string("cfg_file")
-        .context(ErrorKind::ConfigMissingKeys)?;
-    let config_file = config::File::with_name(&config_file_string);
-    config.merge(config_file).context(ErrorKind::ConfigImmutable)?;
-
-    // Apply environment variable overrides
+    // Apply environmenet variable overrides
     let env_vars = Environment::with_prefix("AARDWOLF")
         .separator("_")
         .ignore_empty(true);
-    config.merge(env_vars).context(ErrorKind::ConfigImmutable)?;
+    builder = builder.add_source(env_vars);
+//
+    let log_path = parsed_args.log.unwrap(); 
+    builder = builder
+        .set_override("Log.file", log_path.to_str())
+        .context(ErrorKind::ConfigImmutable)?;
+    
+    //Err(ErrorKind::ConfigImmutable.into())
 
-    // Remove the need for a .env file to avoid defining env vars twice.
-    env::set_var("DATABASE_URL", db_conn_string(&config)?);
-
-    if let Some(log_path) = parsed_args.log {
-        config
-            .set("Log.file", log_path.to_str())
-            .context(ErrorKind::ConfigImmutable)?;
+    match builder.build() {
+        Ok(config) => {
+            env::set_var("DATABASE_URL", db_conn_string(&config)?);
+            return Ok(config)
+        },
+        Err(e) => {
+            // Throw an error
+            return Err(e.into());
+        }
     }
 
-    Ok(config)
+    // Merge config file and apply overrides
+    //let config_file_string = config
+    //    .get_string("cfg_file")
+    //    .context(ErrorKind::ConfigMissingKeys)?;
+    //let config_file = config::File::with_name(&config_file_string);
+    //config.merge(config_file).context(ErrorKind::ConfigImmutable)?;
+
+    // Apply environment variable overrides
+
+    // Remove the need for a .env file to avoid defining env vars twice.
+
 }
 
 pub fn db_conn_string(config: &Config) -> Result<String> {
