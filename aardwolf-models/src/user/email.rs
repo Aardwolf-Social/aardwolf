e67fa3941c2dac588fa771e551afe6bc90be86e9
Banjo Fox <drbanjofox@protonmail.com>
2023-08-30 14:51:43 -0400

    Apply a round of clippy auto-fixes

--- aardwolf-models/src/user/email.rs
+++ aardwolf-models/src/user/email.rs
@@ -188,7 +188,9 @@ impl UnverifiedEmail {
         user: UnverifiedUser,
         token: EmailVerificationToken,
     ) -> Result<(AuthenticatedUser, VerifyEmail), VerificationError> {
-        let res = self.verify(token).map(|verify_email| {
+        
+
+        self.verify(token).map(|verify_email| {
             (
                 AuthenticatedUser {
                     id: user.id,
@@ -199,9 +201,7 @@ impl UnverifiedEmail {
                 },
                 verify_email,
             )
-        });
-
-        res
+        })
     }
 
     pub fn verify(self, token: EmailVerificationToken) -> Result<VerifyEmail, VerificationError> {
